f80b4f210e6916e6bb71d30938760ce95474c010
EduardoAraD <araujocarlos893@gmail.com>
2020-10-14 19:00:02 -0300

    Update OrphanagesController

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -35,7 +35,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
         } = request.body
     
         const orphanagesRepository = getRepository(Orphanage)
@@ -52,7 +52,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends === 'true',
             images
         }
 
